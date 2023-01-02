9fed10ade15bd6cbb66e8406eb27e954a3796cc3
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2023-01-02 16:29:15 -0600

    Make code follow PEP8 style guide and add type annotations

--- AutoClicker.py
+++ AutoClicker.py
@@ -1,78 +1,79 @@
 #!/usr/bin/env python3
-# Threaded auto clicker
 # -*- coding: utf-8 -*-
+# Threaded auto clicker
+
+"Threaded Auto Clicker script"
 
 # Programmed by BitMan64 with minor changes by CoolCat467
 
-import os, time
-from threading import Thread
-
-# To change back to original mode without clicks per seccond
-# regulation, change REGULATE to False.
-REGULATE = True
-
-try:
-    from pynput.mouse import Button, Controller
-    from pynput.keyboard import Listener, KeyCode
-except ImportError:
-    print('Error: pynput not found!')
-    print('Please install pynput for this program to function!')
-    print('Would you like to attempt to automatically install pynput?')
-    autoinstall = input('(y/n) : ').lower() in ('y', 'yes')
-    if autoinstall:
-        print('\nAttempting automatic install...')
-        cmdout = os.system('pip3 install pynput --user')
-        if cmdout == 0:
-            print('pynput installed successfully! Please restart the program.')
-        else:
-            print('Attempted install returned with code %i.' % cmdout, file=os.sys.stderr)
-            autoinstall = False
-    if not autoinstall:
-        print('Please install pynput manually with "pip3 install pynput --user".')
-    sand = input('Press Press Return to Continue. ')
-    os.abort()
+from __future__ import annotations
 
 __title__ = 'Threaded Auto Clicker'
 __author__ = 'BitMan64 & CoolCat467'
-__version__ = '1.1.0'
+__version__ = '1.2.0'
 __ver_major__ = 1
-__ver_minor__ = 1
+__ver_minor__ = 2
 __ver_patch__ = 0
 
-TOGGLEDELAY = 1
 
-def delaySet():
+from typing import Final
+import time
+from threading import Thread
+
+# lintcheck: import-error (E0401): Unable to import 'pynput.mouse'
+from pynput.mouse import Button, Controller
+# lintcheck: import-error (E0401): Unable to import 'pynput.keyboard'
+from pynput.keyboard import Listener, KeyCode, Key
+
+
+# To change back to original mode without clicks per second
+# regulation, change REGULATE to False.
+REGULATE: Final = True
+
+TOGGLEDELAY: Final = 1
+
+
+def delay_set() -> float:
+    """Return clicks per second float from user input"""
     while True:
         try:
-            delay = 1 / int(input('Enter clicks per seccond: '))
+            delay = 1 / int(input('Enter clicks per second: '))
         except ValueError:
             print('Please enter a valid number.\n')
         else:
-            break
-    return delay
+            return delay
+
 
 class ClickMouse(Thread):
     """Thread that uses a mouse object to click a given button with a delay."""
-    def __init__(self, mouse, button, delay=0):
+    def __init__(self,
+                 mouse: Controller,
+                 button: Button,
+                 delay: float = 0.0) -> None:
         Thread.__init__(self)
+        
         self.mouse = mouse
         self.button = button
         self.delay = float(delay)
-        self.lastToggle = 0
-        self.click = False
-        self.active = False
+        self.last_toggle: float = 0
+        self.click: bool = False
+        self.active: bool = False
+        
         self.start()
     
-    def toggle(self):
-        if time.time() + TOGGLEDELAY > self.lastToggle:
-            self.lastToggle = int(time.time())
+    def toggle(self) -> None:
+        """Toggle clicking state"""
+        if time.time() + TOGGLEDELAY > self.last_toggle:
+            self.last_toggle = int(time.time())
             self.click = not self.click
     
-    def exit(self):
+    def exit(self) -> None:
+        """Stop thread"""
         self.click = False
         self.active = False
     
-    def run(self):
+    def run(self) -> None:
+        """Main thread loop"""
         if not REGULATE:
             self.active = True
             while self.active:
@@ -83,8 +84,8 @@ class ClickMouse(Thread):
             self.active = True
             last = time.time()
             count = 0
-            originalDelay = self.delay
-            targetCps = 1 / originalDelay
+            original_delay = self.delay
+            target_clicks_per_sec = 1 / original_delay
             while self.active:
                 if self.click:
                     self.mouse.click(self.button)
@@ -93,38 +94,45 @@ class ClickMouse(Thread):
                     if change >= 1:
                         cps = count / change
                         last = time.time()
-                        if cps != targetCps:
-                            deltacps = targetCps - cps
+                        if cps != target_clicks_per_sec:
+                            deltacps = target_clicks_per_sec - cps
                             self.delay += (1/deltacps)
                     time.sleep(self.delay)
                 else:
                     time.sleep(0.001)
-    pass
 
-def run():
+def run() -> None:
+    """Synchronous Entry Point"""
     mouse = Controller()
     leftclick = Button.left
     rightclick = Button.right
     
-    delay = delaySet()
+    delay = delay_set()
     
     leftclick_thread = ClickMouse(mouse, leftclick, delay)
     rightclick_thread = ClickMouse(mouse, rightclick, delay)
     
-    print('To toggle left clicking, press the "1" key.\nTo toggle right clicking, press the "2" key.\nPress the "3" key to stop clicking.\nPress the "`" key (grave accent) to stop the program.')
+    print('''To toggle left clicking, press the "1" key.
+To toggle right clicking, press the "2" key.
+Press the "3" key to stop clicking.
+Press the "`" key (grave accent) to stop the program.''')
     
-    leftToggle = KeyCode(char='1')
-    rightToggle = KeyCode(char='2')
-    stopClicking = KeyCode(char='3')
+    left_toggle = KeyCode(char='1')
+    right_toggle = KeyCode(char='2')
+    stop_clicking = KeyCode(char='3')
     exit_key = KeyCode(char='`')
     
-    def ToggleKeys():
-        def on_press(key):
-            if key == leftToggle:
+    def toggle_keys() -> None:
+        def on_press(key: Key | KeyCode | None = None) -> None:
+            if key is None:
+                return
+            if isinstance(key, Key):
+                return
+            if key == left_toggle:
                 leftclick_thread.toggle()
-            elif key == rightToggle:
+            elif key == right_toggle:
                 rightclick_thread.toggle()
-            elif key == stopClicking:
+            elif key == stop_clicking:
                 leftclick_thread.click = False
                 rightclick_thread.click = False
             elif key == exit_key:
@@ -132,11 +140,11 @@ def run():
         with Listener(on_press=on_press) as listener:
             listener.join()
     try:
-        ToggleKeys()
+        toggle_keys()
     finally:
         leftclick_thread.exit()
         rightclick_thread.exit()
 
 if __name__ == '__main__':
-    print('%s v%s Programmed by %s.\n' % (__title__, __version__, __author__))
+    print(f'{__title__} v{__version__} Programmed by {__author__}.\n')
     run()
